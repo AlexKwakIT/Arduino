7b4be482217b6cfe6eedae0e1b3eb1789b2f33a5
Alex Kwak <info@alexkwak.nl>
2026-04-12 12:37:55 +0200

    WIP

--- Signals/Signals.ino
+++ Signals/Signals.ino
@@ -10,7 +10,7 @@
 #define TFT_RST  8   // or -1 if tied to Arduino reset
 #define TFT_BL   12  
 
-#define NUM_MESSAGE_LINES 6
+#define MAX_MESSAGE_LINES 20
 
 #define STRAIGHT LOW
 #define TURNOUT  HIGH
@@ -29,11 +29,13 @@ byte i2c_values_cur[NUM_I2C_DEVICES] = {0, 0, 0, 0, 0, 0, 0, 0};
 byte i2c_values_new[NUM_I2C_DEVICES] = {0, 0, 0, 0, 0, 0, 0, 0};
 int max_num_signals = 0;
 
-String messages[NUM_MESSAGE_LINES] = {"", "", "", "", "", ""};
+String messages[MAX_MESSAGE_LINES] = {"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 long lastRefresh = 0;
+
+int led_value = 255;
 
 String setSignal(int signalNr, int color) {
   int index = signalNr * 3 + color;
@@ -68,20 +70,14 @@ int get_pin(int nr) {
 void setup() {
   Wire.begin(); // join I2C bus as master
   Serial.begin(9600);
-  Serial.println("Init...");
   pinMode(LED_BUILTIN, OUTPUT);
 
   pinMode(TFT_BL, OUTPUT);
   analogWrite(TFT_BL, 255);
 
-  // Setup display
   tft.initR(INITR_BLACKTAB);
   tft.setRotation(1);
-  tft.fillScreen(ST77XX_BLUE);
-  tft.setTextColor(ST77XX_YELLOW);
-  tft.setTextSize(1);
-  tft.setCursor(0, 4);
-  tft.println(" Init...");
+  addMessage("Init...");
 
   // Find I2C devices
   int addressIndex = 0;
@@ -95,10 +91,7 @@ void setup() {
         addressIndex += 1;
         String msg = " I2C at 0x";
         msg += String(addr, HEX);
-        Serial.println(msg);
-        tft.println(msg);
         addMessage(msg);
-        printStatus();
       }
   }
   max_num_signals = addressIndex;
@@ -112,10 +105,6 @@ void setup() {
   while (Serial.available() > 0) {
     char ch = Serial.read();
   }
-
-  printStatus();
-
-  Serial.println(i2c_addrs[0]);
 
   Wire.beginTransmission(i2c_addrs[0]);
   Wire.write(0);
@@ -132,97 +121,67 @@ void loop() {
   // internal LED short blink
   digitalWrite(LED_BUILTIN, (millis() % 2000 <= 200 ? HIGH : LOW));
 
-  delay(1000);
+  delay(100);
   Wire.beginTransmission(i2c_addrs[0]);
-  Wire.write(255);
+  Wire.write(led_value);
   Wire.endTransmission();
 
-  delay(1000);
+  delay(100);
   Wire.beginTransmission(i2c_addrs[0]);
   Wire.write(254);
   Wire.endTransmission();
 
-  delay(1000);
-  Wire.beginTransmission(i2c_addrs[0]);
-  Wire.write(253);
-  Wire.endTransmission();
-
-  delay(1000);
-  Wire.beginTransmission(i2c_addrs[0]);
-  Wire.write(251);
-  Wire.endTransmission();
-
-  delay(2000);
-  Wire.beginTransmission(i2c_addrs[0]);
-  Wire.write(255);
-  Wire.endTransmission();
-
-  for (int i=0; i<10; i++) {
-    delay(400);
-    Wire.beginTransmission(i2c_addrs[0]);
-    Wire.write(255);
-    Wire.endTransmission();
-
-    delay(400);
-    Wire.beginTransmission(i2c_addrs[0]);
-    Wire.write(254);
-    Wire.endTransmission();
-
-  }
-
-  // if (lastRefresh + 1000 > millis()) {
-  //   set_I2C();
-  // }
-
-  // recvData();
-  // printStatus();
+  recvData();
 }
 
 void addMessage(String message) {
-  for (int i=NUM_MESSAGE_LINES-2; i>=0; i--) {
+   // 1. Try to find first empty slot
+  for (int i = 0; i < MAX_MESSAGE_LINES; i++) {
+    if (messages[i].length() == 0) {
+      messages[i] = " " + message;
+      printStatus();
+      return;
+    }
+  }
+
+  // 2. If no empty slot: shift everything up
+  for (int i = 0; i < MAX_MESSAGE_LINES - 1; i++) {
+    messages[i] = messages[i + 1];
+  }
+  // 3. Add new text at the end
+  messages[MAX_MESSAGE_LINES - 1] = " " + message;
+  for (int i=MAX_MESSAGE_LINES-2; i>=0; i--) {
     messages[i+1] = messages[i];
   }
-  messages[0] = message;
   printStatus();
 }
 
 void recvData() {
   if (Serial.available() > 0) {
-      char controlChar1 = Serial.read();
-      switch (controlChar1) {
-        case '?':
-          Serial.print("{\"id\":\"eTrack Signals\"}");
-          break;
-        // case 's': {
-        //   // Set switch on/off
-        //   char controlChar2 = Serial.read();
-        //   char controlChar3 = Serial.read();
-        //   char controlChar4 = Serial.read();
-        //   int switchNr = 10 * (controlChar2 - '0') + (controlChar3 - '0');
-        //   String msg = setSwitch(switchNr, controlChar4);
-        //   Serial.print(msg + "\n");
-        // }
-        // break;
-      }
+    led_value = 251;
+    char controlChar1 = Serial.read();
+    switch (controlChar1) {
+      case '?':
+        addMessage("Scan request");
+        Serial.print("{\"id\":\"eTrack Signals\"}");
+        break;
+    }
   }
 }
 
 void printStatus() {
-  // display.setTextSize(2);      // Normal 1:1 pixel scale
-  // display.setTextColor(WHITE); // Draw white text
-  // display.setCursor(0, 0);     // Start at top-left corner
-  // display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  // Setup display
+  tft.fillScreen(ST77XX_BLUE);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setCursor(0, 4);
+  tft.setTextSize(2);
+  tft.print(" ");
+  tft.println(BOARD_PURPOSE);
 
-  // display.clearDisplay();
-  
-  // display.setTextColor(BLACK, WHITE);
-  // display.println(BOARD_PURPOSE);
-
-  // display.setTextColor(WHITE, BLACK);
-  // display.setTextSize(1);
-  // for (String message : messages) {
-  //   display.println(message);
-  // }
-  // display.display();
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_YELLOW);
+  for (String message : messages) {
+    tft.println(message);
+  }
 }
 
---
+++ esp32_c3_detection/esp32_c3_detection.ino
@@ -0,0 +1,146 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+
+#define SDA_PIN 5
+#define SCL_PIN 6
+
+#define SCREEN_WIDTH 72
+#define SCREEN_HEIGHT 40
+
+const char* ssid = "eTrackServer";
+const char* password = "eTrack12321";
+
+const char* serverURL = "http://192.168.1.100:8000/detect/abc/klm/"; // your server
+
+U8G2_SSD1306_72X40_ER_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
+
+// PN532 setup
+PN532_I2C pn532i2c(Wire);
+PN532 nfc(pn532i2c);
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println("Starting debugging");
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  display.begin();
+  display.setFont(u8g2_font_ncenB08_tr);
+  display.clearBuffer();
+  display.drawStr(0, 10, "Init...");
+  display.sendBuffer();
+
+  WiFi.mode(WIFI_MODE_STA);
+  WiFi.setSleep(false);
+  WiFi.setTxPower(WIFI_POWER_8_5dBm);
+  WiFi.begin(ssid, password, 0, nullptr, true);
+  delay(1000);
+
+  display.clearBuffer();
+  display.drawStr(0, 10, "Connect to");
+  display.drawStr(0, 20, ssid);
+  static char rssiStr[10];
+  sprintf(rssiStr, "%d", WiFi.RSSI());
+  display.drawStr(0, 30, rssiStr);
+  display.drawStr(20, 30, "dB");
+  display.sendBuffer();
+
+  unsigned long startTime = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000) {
+    delay(500);
+  }
+
+  display.clearBuffer();
+  if (WiFi.status() != WL_CONNECTED) {
+    display.drawStr(0, 10, "Connection");
+    display.drawStr(0, 20, "FAILED");
+    while (true);
+  } else {
+    display.drawStr(0, 10, "Connected:");
+    display.drawStr(0, 20, ssid);
+    display.sendBuffer();
+  }
+
+  nfc.begin();
+  nfc.SAMConfig();
+
+  Serial.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+  sendUID("foo");
+
+  // Find I2C devices
+  for (byte addr = 0; addr <= 127; addr++) {
+      Wire.beginTransmission(addr);
+      if (Wire.endTransmission() == 0) {
+        Wire.beginTransmission(addr);
+        Wire.write(0x00);             // register to read
+        Wire.endTransmission(false);  // repeated start
+        String msg = " I2C at 0x";
+        msg += String(addr, HEX);
+        Serial.println(msg);
+      }
+  }
+
+}
+
+void loop() {
+
+  uint8_t uid[7];
+  uint8_t uidLength;
+
+  if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength)) {
+    Serial.println("A");
+    Serial.println(uidLength);
+
+    String uidString = "";
+
+    for (int i = 0; i < uidLength; i++) {
+      if(uid[i] < 0x10) uidString += "0";
+      uidString += String(uid[i], HEX);
+    }
+
+    uidString.toUpperCase();
+
+  //   display.clearDisplay();
+  //   display.setTextSize(1);
+  //   display.setCursor(0,0);
+  //   display.println("Card UID:");
+  //   display.println(uidString);
+  //   display.display();
+
+    sendUID(uidString);
+
+    delay(1000);
+  }
+}
+
+void sendUID(String uid) {
+  Serial.print("UID: ");
+  Serial.println(uid);
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    String url = String(serverURL) + uid + "/";
+    http.begin(url);
+    int httpResponseCode = http.GET();
+    if (httpResponseCode > 0) {
+      Serial.print("Response code: ");
+      Serial.println(httpResponseCode);
+
+      String payload = http.getString();
+      Serial.println("Response:");
+      Serial.println(payload);
+    } else {
+      Serial.print("Error: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+  } else{
+      Serial.print("Wrong WiFi status: ");
+      Serial.println(WiFi.status());
+  }
+}
